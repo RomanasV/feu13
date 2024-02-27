1df2277e1835782e722879a7d7d5a9b172209e1a
RomanasV <romanasvenckus@gmail.com>
2024-02-27 10:56:09 +0200

    js basics, variables

---
+++ 2024-02-27-js-basics/script.js
@@ -0,0 +1,33 @@
+// Kintamojo sukūrimas:
+// 1. Iniciavimo žodelis (var, let, const)
+// 2. Kintamojo pavadinimas
+// 3. Lygybės ženklas
+// 4. Kintamojo vertė
+
+var vardas = 'John' // String (tekstas) duomenų tipas
+let pavarde = "Doe" // String (tekstas) duomenų tipas
+let miestas = `Vilnius` // String (tekstas) duomenų tipas
+let amzius = 30 // Number (skaičius) duomenų tipas
+let yraStudentas = false // Boolean (true/false) duomenų tipas
+const asmensKodas = 378645312154 // Number (skaičius) duomenų tipas
+
+console.log(vardas)
+console.log(pavarde)
+console.log(miestas)
+console.log(amzius)
+console.log(yraStudentas)
+
+console.log(vardas, typeof vardas)
+console.log(pavarde, typeof pavarde)
+console.log(miestas, typeof miestas)
+console.log(amzius, typeof amzius)
+console.log(yraStudentas, typeof yraStudentas)
+console.log(asmensKodas, typeof asmensKodas)
+
+console.log(pavarde)
+pavarde = 'Steve'
+console.log(pavarde)
+
+console.log(amzius)
+amzius = 31
+console.log(amzius)
