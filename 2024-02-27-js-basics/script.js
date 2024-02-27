45ca47f5757652f447409071319b1af90c2fa003
RomanasV <romanasvenckus@gmail.com>
2024-02-27 14:42:30 +0200

    conditions

--- 2024-02-27-js-basics/script.js
+++ 2024-02-27-js-basics/script.js
@@ -1,3 +1,5 @@
+console.groupCollapsed('JS Basics')
+
 // Kintamojo sukūrimas:
 // 1. Iniciavimo žodelis (var, let, const)
 // 2. Kintamojo pavadinimas
@@ -39,3 +41,130 @@ console.log(`Hello, ${vardas} ${pavarde}.`)
 
 // John Doe (amžius 25 m.), asmens kodas: 37878787878, gyvenamasis miestas: Kaunas.
 console.log('John Doe (amžius 25 m.), asmens kodas: 37878787878, gyvenamasis miestas: Kaunas.')
+
+console.log(vardas + ' ' + pavarde + ' (amžius ' + amzius + ' m.), asmens kodas: ' + asmensKodas + ', gyvenamasis miestas: ' + miestas + '.')
+
+console.log(vardas + " " + pavarde + " (amžius " + amzius + " m.), asmens kodas: " + asmensKodas + ", gyvenamasis miestas: " + miestas + ".")
+
+console.log(`${vardas} ${pavarde} (amžius ${amzius} m.), asmens kodas: ${asmensKodas}, gyvenamasis miestas: ${miestas}.`)
+
+// John said: "Good morning".
+console.log('John said: "Good morning".')
+console.log(`John said: "Good morning".`)
+// console.log("John said: "Good morning".")
+
+// That's my car.
+console.log("That's my car.")
+console.log(`That's my car.`)
+console.log('That\'s my car.')
+
+// John said: "That's my car".
+console.log(`John said: "That's my car".`)
+console.log("John said: \"That's my car\".")
+console.log('John said: "That\'s my car".')
+
+// John `said`: "That's my car".
+console.log('John `said`: "That\'s my car".')
+console.log("John `said`: \"That's my car\".")
+console.log(`John \`said\`: "That's my car".`)
+
+// John \`said\`: "That's my car".
+console.log('John \\`said\\`: "That\'s my car".')
+console.log("John \\`said\\`: \"That's my car\".")
+console.log(`John \\\`said\\\`: "That's my car".`)
+
+let sentence = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi totam, repellendus neque.'
+
+console.log(sentence)
+console.log(sentence.length) // String simbolių skaičius
+console.log(sentence[0])
+console.log(sentence.charAt(0))
+console.log(sentence.at(0))
+console.log(sentence.toUpperCase())
+console.log(sentence.toLowerCase())
+
+console.groupEnd()
+
+console.groupCollapsed('Math Operators')
+// Matematiniai operatoriai
+let num1 = 22
+let num2 = 10
+let num3 = '10'
+
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+console.log(num1 % num2)
+
+console.log(num1 + num2 * num2)
+console.log((num1 + num2) * num2)
+
+console.log(num1 + num3)
+console.log(22 + '10')
+console.log('22' + '10')
+console.log('2210')
+
+console.log(num1 - num3)
+console.log(num1 * num3)
+console.log(num1 / num3)
+console.log(num1 % num3)
+
+console.log(num1 + num3 * num3)
+console.log(22 + '10' * '10')
+console.log(22 + 100)
+console.log(122)
+
+console.log((num1 + num3) * num3)
+console.log((22 + '10') * '10')
+console.log(('22' + '10') * '10')
+console.log('2210' * '10')
+console.log(22100)
+
+console.groupEnd()
+
+// SĄLYGOS
+// Dviguba lygybė (loosely equal ==): tikrina TIK reikšmes ir nekreipia dėmesio į duomenų tipą.
+console.log(10 == 10) // true
+console.log('vienas' == 'vienas') // true
+console.log('10' == '10') // true
+console.log(10 == '10') // true
+
+// Triguba lygybė (strictly qual ===): pirmiausiai tikrina ar sutampa duomenų tipai, o tada tikrina reikšmes.
+console.log(10 === 10) // true
+console.log('vienas' === 'vienas') // true
+console.log('10' === '10') // true
+console.log(10 === '10') // false
+
+console.log(10 > 10) // false
+console.log(10 < 10) // false
+console.log(10 >= 10) // true
+console.log(10 <= 10) // true
+
+console.log(10 == 10) // true
+console.log(10 != 10) // false
+
+console.log(10 === 10) // true
+console.log(10 !== 10) // false
+
+console.log(true) // true
+console.log(!true) // false
+console.log(false) // false
+console.log(!false) // true
+console.log(!!false) // false
+
+// IF, ELSE IF, ELSE
+let light = 'green'
+
+console.log(light)
+
+if (light === 'green') {
+  console.log('Galima eiti')
+} else if (light === 'yellow') {
+  console.log('Pasiruošk')
+} else if (light === 'red') {
+  console.log('STOP')
+} else {
+  console.log('Sugedo :(')
+}
+
