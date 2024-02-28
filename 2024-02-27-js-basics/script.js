da29ce4375595213a6d88f58b61f68c44626a84d
RomanasV <romanasvenckus@gmail.com>
2024-02-28 14:30:28 +0200

    password task done

--- 2024-02-27-js-basics/script.js
+++ 2024-02-27-js-basics/script.js
@@ -277,8 +277,66 @@ if (personAge < 18) {
   console.log('Pirkti gali')
 }
 
-let age = prompt('Enter your age')
-console.log(age)
+
+// SLAPTAŽODIS:
+// 1. Jeigu slaptažodis yra trumpesnis už 16 simbolių, tai parašyti jog „Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo."
+// 2. Jeigu slaptažodis yra ilgesnis už 15 simbolių, tai:
+// 2.1. Patikrinti ar jis yra ilgesnis už 20 simbolių. Jeigu ilgesnis, tai parašyti: „Slaptažodis tinkamas".
+// 2.2. Jeigu jis nėra ilgesnis už 20 simbolių, tai parašyti jog: „Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo."
+
+// 3. Jeigu slaptažodis neturi grotažymių (#), tai parašyti: „Slaptažodis privalo turėti grotažymes"
+
+let password = 'fsdfsdgflkjsakl'
+let passLength = password.length
+console.log(password)
+console.log(!password.includes('#'))
+console.log(passLength)
+
+// if (passLength < 16) {
+//   console.log('Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo.')
+// } else if (passLength > 15) {
+//   if (passLength > 20) {
+//     console.log('Slaptažodis tinkamas')
+//   } else if (passLength <= 20) {
+//     console.log('Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo.')
+//   }
+// }
+
+// 0. Jeigu nėra # - error
+// 1. 0 - 15
+// 2. 16 - 20
+// 3. 21+
+
+if (!password.includes('#')) {
+  console.log('Slaptažodis privalo turėti grotažymes')
+} else if (passLength < 16) {
+  console.log('Slaptažodis yra per trumpas. Jis privalo būti bent 16 simbolių ilgumo.')
+} else if (passLength < 21) {
+  console.log('Slaptažodis yra tinkamas. Tačiau rekomenduojama jog jis būtų bent 21 simbolio ilgumo.')
+} else {
+  console.log('Slaptažodis tinkamas')
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let age = prompt('Enter your age')
+// console.log(age)
 
 // Kiek bus 123 + 456
 let correctAnswer11 = 579 
