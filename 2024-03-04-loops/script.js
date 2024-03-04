bdf3794481a7d41511047c6bbfdb9153b1262e7e
RomanasV <romanasvenckus@gmail.com>
2024-03-04 09:26:32 +0200

    loop starter

---
+++ 2024-03-04-loops/script.js
@@ -0,0 +1,104 @@
+// console.log('- ' + 1)
+// console.log('- ' + 2)
+// console.log('- ' + 3)
+// console.log('- ' + 4)
+// console.log('- ' + 5)
+// console.log('- ' + 6)
+// console.log('- ' + 7)
+// console.log('- ' + 8)
+// console.log('- ' + 9)
+// console.log('- ' + 10)
+// console.log('- ' + 11)
+// console.log('- ' + 12)
+// console.log('- ' + 13)
+// console.log('- ' + 14)
+// console.log('- ' + 15)
+// console.log('- ' + 16)
+// console.log('- ' + 17)
+// console.log('- ' + 18)
+// console.log('- ' + 19)
+// console.log('- ' + 20)
+
+function count(num) {
+  console.log('- ' + num + '!')
+}
+
+// count(1)
+// count(2)
+// count(3)
+// count(4)
+// count(5)
+// count(6)
+// count(7)
+// count(8)
+// count(9)
+// count(10)
+// count(11)
+// count(12)
+// count(13)
+// count(14)
+// count(15)
+// count(16)
+// count(17)
+// count(18)
+// count(19)
+// count(20)
+
+
+
+// FOR CIKLAS (loop)
+// 1. Iniciavimo žodelis - for
+// 2. Paprasti skliaustai - ()
+// 2.1. Sukuriams kintamasis (dažniausiai jo pavadinimas yra i)
+// 2.2. Sąlyga
+// 2.3. Kintamojo vertės pakeitimas
+// 3. Riestiniai skliaustai - {}
+
+/*
+
+for (kintamasis; sąlyga; kintamojo keitimas) {
+
+}
+
+*/
+
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i)
+// }
+
+// for (let i = 0; i < 20; i++) {
+//   console.log(i)
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//   // console.log('- ' + i)
+//   count(i)
+// }
+
+function loopTest() {
+  for (let i = 1; i <= 20; i++) {
+    count(i)
+  }
+}
+
+// loopTest()
+
+let str = 'Sveikas rytas'
+
+// console.log(str[0])
+// console.log(str[1])
+// console.log(str[2])
+// console.log(str[3])
+// console.log(str[4])
+// console.log(str[5])
+// console.log(str[6])
+// console.log(str[7])
+// console.log(str[8])
+// console.log(str[9])
+// console.log(str[10])
+// console.log(str[11])
+
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i])
+// }
