c26334ee2e5b7da19da773dc6112d6d95f9b5241
RomanasV <romanasvenckus@gmail.com>
2024-03-04 15:29:15 +0200

    fizzBuzz task done

--- 2024-03-04-loops/tasks.js
+++ 2024-03-04-loops/tasks.js
@@ -7,7 +7,7 @@ function task1(start = 1, end = 100, nth = 1) {
   }
 }
 
-task1(10, 50, 5)
+// task1(10, 50, 5)
 
 // 2. Padaugina skaičių iš 5.
 function task2() {
@@ -159,3 +159,65 @@ function task26(start, end, nth) {
 
 // num++
 // console.log(num)
+
+
+function fizzBuzz() {
+  for (let i = 1; i <= 110; i++) {
+
+    if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+      console.log('FizzBuzzBiff')
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+    } else if (i % 3 === 0 && i % 7 === 0) {
+      console.log('FizzBiff')
+    } else if (i % 5 === 0 && i % 7 === 0) {
+      console.log('BuzzBiff')
+    } else if (i % 3 === 0) {
+      console.log('Fizz')
+    } else if (i % 5 === 0) {
+      console.log('Buzz')
+    } else if (i % 7 === 0) {
+      console.log('Biff')
+    } else {
+      console.log(i)
+    }
+
+  }
+}
+
+// fizzBuzz()
+
+
+function fizzBuzz2() {
+  for (let i = 1; i <= 110; i++) {
+    let output = ''
+
+    if (i % 3 === 0) {
+      output += 'Fizz'
+    }
+
+    if (i % 5 === 0) {
+      output += 'Buzz'
+    }
+
+    if (i % 7 === 0) {
+      output += 'Biff'
+    }
+
+    if (i % 9 === 0) {
+      output += 'Fuzz'
+    }
+
+    if (i % 11 === 0) {
+      output += 'Bizz'
+    }
+
+    if (!output) {
+      output = i
+    }
+
+    console.log(output)
+  }
+}
+
+fizzBuzz2()
