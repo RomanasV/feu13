11ee01afb316de39ccce6fb14187c10436724b0c
RomanasV <romanasvenckus@gmail.com>
2024-03-05 08:43:13 +0200

    array tasks

--- 2024-03-04-arrays/script.js
+++ 2024-03-04-arrays/script.js
@@ -45,7 +45,7 @@ console.log(newArray)
 let cities = ['Vilnius', 'Kaunas', 'Klaipėda', 'Šiaulia', 'Panevėžys'];
 console.log(cities)
 
-// pop, push, shift ir unshift metodai modifikuoja (mutuoja) originalų masyvą.
+// pop, push, shift, unshift ir splice metodai modifikuoja (mutuoja) originalų masyvą.
 
 // pop() metodas - pašalina paskutinį masyvo narį.
 let lastCity = cities.pop()
@@ -53,3 +53,57 @@ console.log(lastCity)
 
 cities.pop()
 console.log(cities)
+
+// shift() metodas - pašalina pirmą masyvo narį.
+let firstCity = cities.shift()
+console.log(firstCity)
+
+cities.shift()
+console.log(cities)
+
+// push() metodas - prideda naują narį (narius) į masyvo pabaigą.
+let updatedCitiesLength = cities.push('Vilnius')
+console.log(updatedCitiesLength)
+
+cities.push('Šîauliai')
+cities.push('Kaunas', 'Panevėžys')
+
+console.log(cities)
+
+// unshift() metodas - prideda naują narį (narius) į masyvo pradžią.
+let updatedCitiesLength2 = cities.unshift('Nida')
+console.log(updatedCitiesLength2)
+
+cities.unshift('Tauragė', 'Palanga')
+
+console.log(cities)
+
+// SPLICE - modifikuoja (mutuoja) originalų masyvą
+let nums = [  1  ,   2  ,   3  ,   4  ,   5  ,   6  ,   7  ,   10  ];
+// Index      0      1      2      3      4      5      6      7
+//         0     1      2      3      4      5      6      7       8
+//        -8    -7     -6     -5     -4     -3     -2     -1
+
+console.log(nums)
+
+// let splicedNums = nums.splice(2)
+// let splicedNums = nums.splice(5)
+// let splicedNums = nums.splice(0)
+// let splicedNums = nums.splice(-2)
+// let splicedNums = nums.splice(-5)
+// let splicedNums = nums.splice(10)
+// let splicedNums = nums.splice(-10)
+// let splicedNums = nums.splice(0, 2)
+// let splicedNums = nums.splice(2, 1)
+// let splicedNums = nums.splice(2, 3)
+// let splicedNums = nums.splice(-3, 2)
+// let splicedNums = nums.splice(-3, 2, 2)
+// let splicedNums = nums.splice(2, 1, 50)
+// let splicedNums = nums.splice(0, 1, 30)
+// let splicedNums = nums.splice(-1, 1, 100)
+// let splicedNums = nums.splice(-3, 2, 40, 60)
+// let splicedNums = nums.splice(0, 0, 0.5, 0.8)
+let splicedNums = nums.splice(-1)
+
+console.log(splicedNums)
+console.log(nums)
