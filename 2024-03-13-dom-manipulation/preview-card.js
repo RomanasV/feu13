c0e1e4c563a961233490fe3f2847173a296b244b
RomanasV <romanasvenckus@gmail.com>
2024-03-13 10:19:23 +0200

    dom task

---
+++ 2024-03-13-dom-manipulation/preview-card.js
@@ -0,0 +1,34 @@
+let cardsList = document.querySelector('.cards-list')
+
+let card = document.createElement('div')
+card.classList.add('card')
+
+cardsList.append(card)
+
+{/* 
+
+<div class="card">
+  <div class="image-wrapper">
+    <img class="product-image" src="images/image-product-desktop.jpg" alt="">
+  </div>
+
+  <div class="content-wrapper">
+    <span class="product-category">Perfume</span>
+    
+    <h2 class="product-title">Gabrielle Essence Eau De Parfum</h2>
+
+    <p class="product-description">A floral, solar and voluptuous interpretation composed by Olivier Polge, Perfumer-Creator for the House of CHANEL.</p>
+
+    <div class="product-price-wrapper">
+      <span class="product-price">$149.99</span>
+      <span class="product-price-old">$169.99</span>
+    </div>
+
+    <button class="add-to-cart-button">
+      <img src="icon-cart.svg" alt="">
+      Add to Cart
+    </button>
+  </div>
+</div> 
+
+*/}
